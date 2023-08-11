dd2463e2c701f0604d597300f691ebe70f6fb47d
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-08-11 10:34:52 +0200

    nav + hero + personalInfo env var

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+export const personalInfo = {
+  firstname: "Hugo",
+  lastname: "COLLIN",
+  linkedin: "https://www.linkedin.com/in/hugocollin/",
+  github: "https://github.com/Hugo-COLLIN",
+  youtube: "https://www.youtube.com/@hugocollin",
+  twitter: "https://twitter.com/hugocoll1n",
+  reddit: "https://www.reddit.com/user/hugocoll1n",
+  tiktok: "https://www.tiktok.com/@hugocoll1n",
+}
